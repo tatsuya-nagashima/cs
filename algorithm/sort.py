541e1e5df6b26361c2f22cb0903f4f8f2ae620cf
tatsuya-nagashima <ta28.nagash1ma@gmail.com>
2018-02-19 12:31:48 +0900

    Add algorithm/selectionSort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -5,8 +5,30 @@ def bubbleSort(arr):
                 tmp      = arr[j]
                 arr[j]   = arr[j+1]
                 arr[j+1] = tmp
+
     return arr
 
-if __name__ == '__main__':
-    arr = [5,4,3,2,1]
-    print bubbleSort(arr)
+def selectionSort(arr):
+    for i in range(len(arr)-1):
+        min=i
+        for j in range (i+1, len(arr)-1):
+            if arr[min] > arr[j]:
+                min = j
+            tmp      = arr[i]
+            arr[i]   = arr[min]
+            arr[min] = tmp
+
+    return arr
+
+
+
+
+if __name__ == "__main__":
+    arr = [5,4,3,2,1,6,7,8,9,10]
+
+    print("before", arr)
+    #bubbleSort(arr)
+    #selectionSort(arr)
+    #nsertionSort(arr)
+    #quickSort(arr)
+    print("after ", arr)
