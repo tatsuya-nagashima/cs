b0eb3f6027d296d4b04e1c80593d95eda4617dc0
tatsuya-nagashima <ta28.nagash1ma@gmail.com>
2018-03-18 12:46:56 +0900

    Add nega-posi

---
+++ image_prodessing/nega-posi.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+from PIL import Image
+import numpy
+img = Image.open("sample.png")
+
+#RGBに変換
+rgb_img = img.convert('RGB')
+
+#元画像のサイズを取得
+#ネガポジ変換用に空画像を作成
+size = rgb_img.size
+img_invert = Image.new('RGB',size)
+
+for x in range(size[0]):
+    for y in range(size[1]):
+        #ピクセルを取得
+        r,g,b = rgb_img.getpixel((x,y))
+
+        #ネガポジ変換
+        r = 255 - r
+        g = 255 - g
+        b = 255 - b
+
+        #img_invertにピクセルをセット
+        img_invert.putpixel((x,y),(r,g,b))
+
+img_invert.show()
